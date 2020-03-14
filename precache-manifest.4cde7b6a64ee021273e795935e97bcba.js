2f2256a0920f3b423d83f677f8fe2ede1e0215b1
bishoy adly <tobbeywilliam@gmail.com>
2020-03-14 17:25:35 +0200

    Updates

---
+++ precache-manifest.4cde7b6a64ee021273e795935e97bcba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186a61b6125703662bfb96810fb58822",
+    "url": "/guestbook-frontend/index.html"
+  },
+  {
+    "revision": "bb044f40340070b3bc30",
+    "url": "/guestbook-frontend/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "9a1605329a587cd48734",
+    "url": "/guestbook-frontend/static/css/main.e41176e0.chunk.css"
+  },
+  {
+    "revision": "bb044f40340070b3bc30",
+    "url": "/guestbook-frontend/static/js/2.20a5e11c.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/guestbook-frontend/static/js/2.20a5e11c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1605329a587cd48734",
+    "url": "/guestbook-frontend/static/js/main.9cea6c57.chunk.js"
+  },
+  {
+    "revision": "729a43717fd5b255b37d",
+    "url": "/guestbook-frontend/static/js/runtime-main.a37bd7b6.js"
+  }
+]);
